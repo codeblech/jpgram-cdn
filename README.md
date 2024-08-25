f3d9e70973d6b8ce7a6e19f38dd5c548fa1a243b
Harsh Sharma <harsh@codelif.in>
2024-08-25 19:24:18 +0530

    make login optional

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Here lies the posts thee shall receive.
 ## How to update image cache?
 Run the `update.sh` script while in root directory of the repository.
 
-You also have to provide a dummy instagram account (to scrape).
+You can also provide a dummy instagram account if you are having problems.
 
 Create a file named `secrets.env` as such in the root directory (it is gitignored so you don't need to worry) 
 ```.env
@@ -13,11 +13,11 @@ JPGRAM_IG_ID=username
 JPGRAM_IG_PSWD=password
 ```
 
-Now, you can run the script `bash update.sh`
+Now, you can run the script `bash update.sh login`
 
 If you don't want to create a secrets file then you can provide the variable while running the script as such 
 ```console
-JPGRAM_IG_ID=username JPGRAM_IG_PSWD=password bash update.sh
+JPGRAM_IG_ID=username JPGRAM_IG_PSWD=password bash update.sh login
 ```
 but this can be a security concern if your shell saves command history.
 
--- update.sh
+++ update.sh
@@ -3,6 +3,13 @@
 PWD="$(pwd)"
 RED='\033[1;31m'
 NC='\033[0m'
+LOGIN=0
+
+if [ "$1" = "login" ];
+then
+  LOGIN = 1
+fi
+
 
 if [ "$( basename $PWD )" != "jpgram-cdn" ];
 then
@@ -12,16 +19,26 @@ fi
 
 source secrets.env 2> /dev/null 1>&2
 
-if [ -z "${JPGRAM_IG_ID}" ] || [ -z "${JPGRAM_IG_PSWD}" ];
+if [ $LOGIN -eq 1 ];
 then
-  printf "${RED}ERROR${NC}: JPGRAM_IG_ID or JPGRAM_IG_PSWD env are not provided.\n" >&2 
-  exit 1
+  if [ -z "${JPGRAM_IG_ID}" ] || [ -z "${JPGRAM_IG_PSWD}" ];
+  then
+    printf "${RED}ERROR${NC}: JPGRAM_IG_ID or JPGRAM_IG_PSWD env are not provided.\n" >&2 
+    exit 1
+  fi
 fi
 
 mkdir -p images && cd images 
 
 printf "Caching images... (this will take a few minutes)\n(To check instaloader output enter 'tail -f images/instaloader.log' in a seperate terminal)\n"
-python3 -m instaloader +../clubs.txt --fast-update --no-videos --no-metadata-json --login $JPGRAM_IG_ID --password $JPGRAM_IG_PSWD 2> instaloader.log >&2
+
+if [ $LOGIN -eq 1 ];
+then
+  python3 -m instaloader +../clubs.txt --fast-update --no-videos --no-metadata-json --login $JPGRAM_IG_ID --password $JPGRAM_IG_PSWD 2> instaloader.log >&2
+else
+  python3 -m instaloader +../clubs.txt --fast-update --no-videos --no-metadata-json 2> instaloader.log >&2
+fi
+
 
 # Generating indices
 cd ..
