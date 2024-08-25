9c3d76732646649618e0d3f4ad9031babce87f4c
codeblech <my.malikyash@gmail.com>
2024-08-25 19:29:10 +0530

    add time to commit message

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 if [ "$( basename $PWD )" != "jpgram-cdn" ];
 then
-  printf "${RED}ERROR${NC}: run this script in root directory of jpgram-cdn repo.\n" >&2 
+  printf "${RED}ERROR${NC}: run this script in root directory of jpgram-cdn repo.\n" >&2
   exit 1
 fi
 
@@ -14,11 +14,11 @@ source secrets.env 2> /dev/null 1>&2
 
 if [ -z "${JPGRAM_IG_ID}" ] || [ -z "${JPGRAM_IG_PSWD}" ];
 then
-  printf "${RED}ERROR${NC}: JPGRAM_IG_ID or JPGRAM_IG_PSWD env are not provided.\n" >&2 
+  printf "${RED}ERROR${NC}: JPGRAM_IG_ID or JPGRAM_IG_PSWD env are not provided.\n" >&2
   exit 1
 fi
 
-mkdir -p images && cd images 
+mkdir -p images && cd images
 
 printf "Caching images... (this will take a few minutes)\n(To check instaloader output enter 'tail -f images/instaloader.log' in a seperate terminal)\n"
 python3 -m instaloader +../clubs.txt --fast-update --no-videos --no-metadata-json --login $JPGRAM_IG_ID --password $JPGRAM_IG_PSWD 2> instaloader.log >&2
@@ -31,7 +31,7 @@ printf "done\n"
 
 if [ $? -ne 0 ]
 then
-  printf "${RED}ERROR${NC}: Cache Updation was unsuccessful. Any changes were not commited and left as it is. Check instaloader.log for furthur details.\n" >&2 
+  printf "${RED}ERROR${NC}: Cache Updation was unsuccessful. Any changes were not commited and left as it is. Check instaloader.log for furthur details.\n" >&2
   read -p "Do you still want to commit the changes? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -42,7 +42,8 @@ fi
 
 printf "Commiting changes... "
 git add .
-git commit -m "chore: update image cache ($( date +%Y-%m-%d ))"
+git commit -m "chore: update image cache ($(date "+%Y-%m-%d--%H-%M-%S"))"
+
 
 
 printf "done.\n"
